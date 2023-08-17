a9f7b4cf359e952c6936d959928d84a9a9c29f50
vonhyou <moi@rfung.dev>
2023-08-17 19:42:40 -0300

    add comments

--- src/main/scala/DataFrameUtils.scala
+++ src/main/scala/DataFrameUtils.scala
@@ -5,6 +5,15 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object DataFrameUtils {
 
+  /**
+   * This method reads the data from the given path and returns a DataFrame
+   * dropping the unnecessary columns and adding the feature vector column
+   *
+   * @param sc    SparkSession
+   * @param path  Path to the csv file
+   * @param isVpn 0.0 if the data is not VPN, 1.0 if the data is VPN
+   * @return DataFrame
+   */
   def getCleanedDataFrame(sc: SparkSession, path: String, isVpn: Double): DataFrame =
     val rawDataFrame = IoUtils.readDataFrameFromCSV(path, sc)
     val filteredDataFrame = filterDataFrame(rawDataFrame)
@@ -13,7 +22,15 @@ object DataFrameUtils {
     IoUtils.printDataFrame(featuredDataFrame)
     featuredDataFrame
 
-  private def addFeaturesToDataFrame(df: DataFrame): DataFrame =
+  /**
+   * This method adds the features to the given DataFrame by collecting
+   * all numeric features except the isVPN column and assembling them to a vector column
+   *
+   * @param df DataFrame to add features
+   * @return DataFrame with a vector feature column
+   */
+  private def addFeaturesToDataFrame(df: DataFrame) =
+    // Get all numeric features except the isVPN column, which is the label
     val numericFeatures = df
       .schema
       .filterNot(st => st.dataType.equals(StringType) || st.name.equals("isVPN"))
@@ -30,7 +47,19 @@ object DataFrameUtils {
       "srcMacLbl_dstMacLbl", "ethVlanID", "hdrDesc")
     .na.drop()
 
+  /**
+   * This method combines the given DataFrames and splits them into training and testing data
+   *
+   * @param df1 DataFrame 1
+   * @param df2 DataFrame 2
+   * @return a tuple of training and testing DataFrames
+   */
   def getSplitData(df1: DataFrame, df2: DataFrame): (DataFrame, DataFrame) =
+    /**
+     * The ratio is calculated as 70% of the minimum of the two DataFrames
+     *
+     * @return a tuple of ratios for df1 and df2
+     */
     def getRatio: (Double, Double) =
       val trainingDataCount = Math.min(df1.count(), df2.count()) * 0.7
       (trainingDataCount / df1.count(), trainingDataCount / df2.count())
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,17 +9,12 @@ import org.apache.spark.sql.SparkSession
   val cleanedNoVpnDf = DataFrameUtils.getCleanedDataFrame(sc, "data/train_no_vpn.csv", 0)
   val cleanedVpnDf = DataFrameUtils.getCleanedDataFrame(sc, "data/train_vpn.csv", 1)
 
+  // combine dataframes and split into training and testing data
   val (trainingData, testingData) = DataFrameUtils.getSplitData(cleanedNoVpnDf, cleanedVpnDf)
 
-  val classifier = new RandomForestClassifier()
-    .setLabelCol("isVPN")
-    .setFeaturesCol("features")
-    .setImpurity("gini")
-    .setMaxDepth(3)
-    .setNumTrees(20)
-    .setFeatureSubsetStrategy("auto")
-
+  val classifier = getRandomForestClassifier("isVPN")
   val model = classifier.fit(trainingData)
+
   val predications = model.transform(testingData)
   val evaluator = new MulticlassClassificationEvaluator()
     .setLabelCol("isVPN")
@@ -32,3 +27,11 @@ private def getSparkSession(name: String): SparkSession = SparkSession.builder()
   .appName(name)
   .config("spark.master", "local")
   .getOrCreate()
+
+private def getRandomForestClassifier(labelCol: String) = new RandomForestClassifier()
+  .setLabelCol(labelCol)
+  .setFeaturesCol("features")
+  .setImpurity("gini")
+  .setMaxDepth(3)
+  .setNumTrees(20)
+  .setFeatureSubsetStrategy("auto")
